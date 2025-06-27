a7dde18ff4f99d7d632756c84aa06ed6c69306fc
ciemilsalim <emilsalimramadhan@gmail.com>
2025-06-27 11:23:14 +0800

    perbaikan teks input

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
 @props(['disabled' => false])
 
 {{-- PERBAIKAN: Mengubah warna latar dan border pada dark mode agar lebih kontras --}}
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-slate-600 dark:bg-slate-800 dark:text-gray-300 focus:border-sky-500 dark:focus:border-sky-600 focus:ring-sky-500 dark:focus:ring-sky-600 rounded-md shadow-sm transition']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-slate-300 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-slate-500 dark:border-slate-600 dark:bg-slate-700 dark:text-gray-300 dark:focus:border-sky-600 dark:focus:ring-sky-600 focus:outline-none focus:ring-1 transition']) !!}>
